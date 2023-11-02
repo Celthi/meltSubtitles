87ccd0493ab3cee1e300aed63f37670943f0df61
AsukaMinato <i@asukaminato.eu.org>
2023-11-02 09:16:18 +0900

    black && typing && isort

--- meltsubtitles/main.py
+++ meltsubtitles/main.py
@@ -1,11 +1,14 @@
-import requests
-import lxml.html as htmlparser
-import re
 import os
-from utils import parse_args, mkdir
-from wordsRepoProc import build_wordrepo
+import re
+from pathlib import Path
+from typing import Any, Mapping
 
-__author__ = 'celhipc'
+import lxml.html as htmlparser
+import requests
+from utils import parse_args
+from wordsRepoProc import build_word_repository
+
+__author__ = "celhipc"
 
 
 def translate2chinese(word):
@@ -15,8 +18,8 @@ def translate2chinese(word):
     :return meanning:
 
     """
-    meanning = ''
-    url = 'http://dict.youdao.com/search?q='
+    meanning = ""
+    url = "http://dict.youdao.com/search?q="
     webpage = get_page(url, word)
     htmltree = htmlparser.fromstring(webpage)
     meanningList = htmltree.xpath('//div[@class="trans-container"]/ul/li')
@@ -32,12 +35,11 @@ def translate2english(word):
     :param words:
     :return:
     """
-    meanning = ''
-    url = 'http://dict.youdao.com/search?q='
+    meanning = ""
+    url = "http://dict.youdao.com/search?q="
     webpage = get_page(url, word)
     htmltree = htmlparser.fromstring(webpage)
-    meanningList = htmltree.xpath(
-        '//*[@id="tEETrans"]/div/ul//*[@class="def"]')
+    meanningList = htmltree.xpath('//*[@id="tEETrans"]/div/ul//*[@class="def"]')
     if len(meanningList) != 0:
         meanning = meanningList[0].text
 
@@ -57,51 +59,54 @@ def get_page(url, word):
     return response.content
 
 
-def run(config):
+def run(config: Mapping[str, Any]):
     """
     main process
     """
     ##
     # build the words repo
-    wordsRepo = build_wordrepo(config['files'])
-    for subtitleFile in config['subtitle']:
-        if not subtitleFile.endswith('.srt'):
+    wordsRepo = build_word_repository(config["files"])
+    for subtitleFile in config["subtitle"]:
+        if not subtitleFile.endswith(".srt"):
             pass
 
         srtfile = subtitleFile
-        with open(srtfile, 'r', encoding='utf-8') as finput:
-            lan = 'ch'
-            if not config['ch']:
-                lan = 'en'
+        with open(srtfile, "r", encoding="utf-8") as finput:
+            lan = config.get("ch", "en")
             subMelted = os.path.join(
-                config['dir'], srtfile[:-4] + '.word.' + lan + '.srt')
+                config["dir"], srtfile[:-4] + ".word." + lan + ".srt"
+            )
             unfamilar = os.path.join(
-                config['words'], "unknown." + '.word.' + lan + '.srt')
+                config["words"], "unknown." + ".word." + lan + ".srt"
+            )
             for index, line in enumerate(finput):
-                if line and not line[0].isdigit() and line != '\n':
+                if line and not line[0].isdigit() and line != "\n":
                     words = re.split(r"[^a-zA-Z']+", line)
                     hasUnknown = False
-                    meanning = ''
+                    meanning = ""
                     for word in words:
-                        if word and word[0].islower() and word not in wordsRepo and "'" not in word:
+                        if (
+                            word
+                            and word[0].islower()
+                            and word not in wordsRepo
+                            and "'" not in word
+                        ):
                             hasUnknown = True
-                            if config['ch']:
-                                meanning += word + ": " + \
-                                    translate2chinese(word) + '\n'
+                            if config["ch"]:
+                                meanning += word + ": " + translate2chinese(word) + "\n"
                             else:
-                                meanning += word + ": " + \
-                                    translate2english(word) + '\n'
+                                meanning += word + ": " + translate2english(word) + "\n"
 
                     if hasUnknown:
-                        if not config['sectime']:
-                            with open(subMelted, 'a', encoding='utf-8') as fouput:
+                        if not config["sectime"]:
+                            with open(subMelted, "a", encoding="utf-8") as fouput:
                                 fouput.write(line)
-                        with open(subMelted, 'a', encoding='utf-8') as fouput:
+                        with open(subMelted, "a", encoding="utf-8") as fouput:
                             fouput.write(meanning)
-                        with open(unfamilar, 'a', encoding='utf-8') as unfamilarWords:
+                        with open(unfamilar, "a", encoding="utf-8") as unfamilarWords:
                             unfamilarWords.write(meanning)
                 else:
-                    with open(subMelted, 'a', encoding='utf-8') as fouput:
+                    with open(subMelted, "a", encoding="utf-8") as fouput:
                         fouput.write(line)
 
 
@@ -113,21 +118,20 @@ def main():
 
     # parse argument
     args = parse_args()
-    config = {}
-    config['subtitle'] = args.subtitle
-    config['sectime'] = args.sec
-    config['files'] = args.wordsrepo
-    config['dir'] = args.path
-    config['ch'] = args.ch
-    config['words'] = args.words
-    if not os.path.exists(config['words']):
-        mkdir(config['words'])
-    if not os.path.exists(config['dir']):
-        mkdir(config['dir'])
+    config = {
+        "subtitle": args.subtitle,
+        "sectime": args.sec,
+        "files": args.wordsrepo,
+        "dir": args.path,
+        "ch": args.ch,
+        "words": args.words,
+    }
+    Path(config["words"]).mkdir(parents=True, exist_ok=True)
+    Path(config["dir"]).mkdir(parents=True, exist_ok=True)
     run(config)
 
 
-if __name__ == '__main__':
-    print('procwssing subtitle')
+if __name__ == "__main__":
+    print("processing subtitle")
     main()
-    print('finished procwssing subtitle')
+    print("finished procwssing subtitle")
--- meltsubtitles/utils.py
+++ meltsubtitles/utils.py
@@ -1,22 +1,6 @@
 import argparse
-import errno
-import os
 
-__author__ = 'celhipc'
-
-
-def mkdir(path, mode=0o777):
-    """
-    Create subdirectory hierarchy given in the paths argument.
-    Ripped from https://github.com/coursera-dl/
-    """
-    try:
-        os.makedirs(path, mode)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
+__author__ = "celhipc"
 
 
 def parse_args():
@@ -25,49 +9,46 @@ def parse_args():
     :return args:
     """
 
-    parser = argparse.ArgumentParser(description='melt subtitles')
+    parser = argparse.ArgumentParser(description="melt subtitles")
 
-    parser.add_argument('subtitle',
-                        nargs='+',
-                        help='subtitle')
+    parser.add_argument("subtitle", nargs="+", help="subtitle")
 
-    parser.add_argument('-e',
-                        dest='ch',
-                        action='store_false',
-                        default=True,
-                        help='指定中文释义还是英文释义')
+    parser.add_argument(
+        "-e", dest="ch", action="store_false", default=True, help="指定中文释义还是英文释义"
+    )
 
-    parser.add_argument('-2',
-                        dest='sec',
-                        action='store_true',
-                        default=False,
-                        help='是否二刷')
+    parser.add_argument(
+        "-2", dest="sec", action="store_true", default=False, help="是否二刷"
+    )
 
-    parser.add_argument('-w',
-                        dest='wordsrepo',
-                        action='store',
-                        nargs='+',
-                        default=['./wordsRepo/en5000x.csv'],
-                        help='specify the words repo')
+    parser.add_argument(
+        "-w",
+        dest="wordsrepo",
+        action="store",
+        nargs="+",
+        default=["./wordsRepo/en5000x.csv"],
+        help="specify the words repo",
+    )
 
-    parser.add_argument('-p',
-                        dest='path',
-                        action='store',
-                        default='.',
-                        help='path to save the files')
-    parser.add_argument('-u',
-                        dest='words',
-                        action='store',
-                        default='./unfamilarWords',
-                        help='file to save unfamilar words')
+    parser.add_argument(
+        "-p", dest="path", action="store", default=".", help="path to save the files"
+    )
+    parser.add_argument(
+        "-u",
+        dest="words",
+        action="store",
+        default="./unfamilarWords",
+        help="file to save unfamilar words",
+    )
 
-    parser.add_argument('-o',
-                        '--overwrite',
-                        dest='overwrite',
-                        action='store_true',
-                        default=False,
-                        help='whether existing files should be overwritten'
-                             ' (default: False)')
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        dest="overwrite",
+        action="store_true",
+        default=False,
+        help="whether existing files should be overwritten" " (default: False)",
+    )
 
     args = parser.parse_args()
 
